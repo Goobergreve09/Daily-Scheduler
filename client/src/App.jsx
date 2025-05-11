3f484319e9f0b131fd1ea137601185c9ea3c887b
Goobergreve09 <gregory.greve@yahoo.com>
2025-05-11 12:06:23 -0400

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import Navbar from "./components/Navbar";
 import React from "react";
 
 const httpLink = createHttpLink({
-  uri: process.env.MONGODB_URI || "http://localhost:3001/graphql",
+  uri: process.env.REACT_APP_GRAPHQL_URI || "http://localhost:3001/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/localgamesdatabase');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/localgamesdatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('✅ MongoDB connected successfully');
+});
+
+module.exports = db;
